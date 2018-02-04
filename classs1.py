f13b3f0f50fd96390fd85780b3e0d8979227cc48
Manasa Kishtapati <manasareddy823@gmail.com>
2018-02-04 18:13:01 +0530

    first commit

---
+++ classs1.py
@@ -0,0 +1,22 @@
+message="helleo World"
+message1='hello'
+print(len(message))
+print(message)
+print(message.lower())
+print("hello"+"\t"+"world"+"  "+"good")
+#print(dir(str))
+message="hello"
+message=message.upper()
+print(message)
+
+#print('hello World!')
+greeting='hi'
+message2=print("manasa.upper()"+" "+"says "+greeting)
+#print using format method
+message='{} {} {}'.format('manasa','says',greeting.upper())
+#using fstrings
+message_hi=f"hello {greeting} "
+print(message_hi)
+#print(message1)
+#print(message)
+
